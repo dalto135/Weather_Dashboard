373124fbdc45f72008f60597a10d958e70c7dc9a
dalto135 <67397294+dalto135@users.noreply.github.com>
2021-03-03 02:35:31 -0500

    edited search functionality

--- Develop/script.js
+++ Develop/script.js
@@ -2,12 +2,14 @@ let body = document.querySelector("body");
 let input = document.querySelector("input");
 let button = document.querySelector("button");
 let search = document.querySelector("#search");
+let current = document.querySelector("#current");
 
 let script = document.createElement("script");
 
 button.addEventListener("click", function() {
     script.setAttribute("src", "http://api.openweathermap.org/data/2.5/weather?q={" + input.value + "}&appid={de53a40654766cb8ce20288a99c9f736}");
     // script.setAttribute("src", "http://api.openweathermap.org/data/2.5/weather?q={" + input.value + "}&appid={1e3c0d8f0165df5cb4048999328d8b79}");
+    current.children[0].innerHTML = input.value;
     let history = document.createElement("button");
     history.innerHTML = input.value;
     search.appendChild(history);
